d7283f7e79bb4880b19a8f366ccd2618bc4d2143
kangalah <nyongesajoannanjala@gmail.com>
2020-07-19 18:35:28 +0300

    create a test_writer.py file

---
+++ tests/test_writer.py
@@ -0,0 +1,17 @@
+import unittest
+from app.models import Writer
+
+class WriterModelTest(unittest.TestCase):
+
+    def setUp(self):
+        self.new_writer = Writer(password = '123')
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_writer.pass_secure is not None)
+
+    def test_no_access_password(self):
+            with self.assertRaises(AttributeError):
+                self.new_writer.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_writer.verify_password('123'))
