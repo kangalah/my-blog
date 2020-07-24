1c21d5eaeb0feb0c2e9fa531ef0a3fc531eebbfe
kangalah <nyongesajoannanjala@gmail.com>
2020-07-24 15:32:27 +0300

    add secret-key

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
 import os
 
 class Config:
-    SECRET_KEY = os.urandom(15)
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:Access@localhost/postblog'
+   
+    SECRET_KEY = '\x84\x14\x10KH\xfb\xb4\x18\xe6\x08"\x95\x1f4\xda\xa8\xe6\xdb\xaf\xb8]\xc4\xfe5'
+    WTF_CSRF_ENABLED = True
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
+    
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blogpost'
+    
     DEBUG = True
 
 config_options ={"production":ProdConfig,"default":DevConfig}
